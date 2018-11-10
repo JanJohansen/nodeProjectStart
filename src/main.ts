55cbd7c364db8513ab988e9326cb6ef7ac863be8
Jan Johansen <jan@johansenweb.dk>
2018-11-10 09:13:53 +0100

    Added abillity to attach debugging session in VSCode to autostarting program.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,33 @@
- console.log("Program is running again!");
+ //*****************************************************************************
+// Enable user to properly close down server...
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+
+process.stdin.on('data', function (text) {
+    //log.info('received data:', text);
+    if (text == "q\r\n") {
+        handleNormalExit();    // TODO: Doesn't seem to work!?
+    }
+    handleNormalExit(); // Use "AnyKey" for now!
+});
+
+function handleNormalExit() {
+    console.log('User shut down.');
+    process.exit();
+}
+ 
+import * as express from "express"
+var app = express()
+const port = 3000
+let count = 0
+
+app.get('/', (req, res) => {
+    res.send('Hello World!!' + count++)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`)
+})
+
+console.log("Programs is running again!");
+ 
